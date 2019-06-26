ead525862a8b30c204ac494b0c35dae77eba705b
drekaygran <akgranbery@gmail.com>
2019-06-26 14:11:21 -0400

    Create and test first version of authentication
    Still need to hide/show the sign up bar and sign out bar based on
    user's login status.

---
+++ assets/scripts/auth/events.js
@@ -0,0 +1,44 @@
+'use strict'
+
+const getFormFields = require('./../../../lib/get-form-fields')
+const api = require('./api')
+const ui = require('./ui')
+
+const onSignUp = event => {
+  event.preventDefault()
+  const formData = getFormFields(event.target)
+  api.signUp(formData)
+    .then(ui.signUpSuccess)
+    .catch(ui.signUpFail)
+}
+
+const onSignIn = event => {
+  event.preventDefault()
+  const formData = getFormFields(event.target)
+  api.signIn(formData)
+    .then(ui.signInSuccess)
+    .catch(ui.signInFail)
+}
+
+const onChangePassword = event => {
+  event.preventDefault()
+  const formData = getFormFields(event.target)
+  console.log('onChangePassword ran')
+  api.changePassword(formData)
+    .then(ui.changePasswordSuccess)
+    .catch(ui.changePasswordFail)
+}
+
+const onSignOut = event => {
+  event.preventDefault()
+  api.signOut()
+    .then(ui.signOutSuccess)
+    .catch(ui.signOutFail)
+}
+
+module.exports = {
+  onSignUp,
+  onSignIn,
+  onChangePassword,
+  onSignOut
+}
